0ac519ddb0195e161f76f6eaa84123b23264efd9
Emre Çolak <emrecolak@Emre.home>
2023-03-27 22:16:00 +0300

    create request validation middleware

--- server/src/routes/auth/signup.ts
+++ server/src/routes/auth/signup.ts
@@ -2,6 +2,7 @@
 import express, {Request, Response} from 'express'
 import { body, validationResult } from 'express-validator'
 import { RequestValidationError } from '../../errors/request-validation-error'
+import { validateRequest } from '../../middlewares/validate-request'
 // Create router
 const router = express.Router()
 
@@ -14,12 +15,9 @@ router.post('/api/users/signup',
             .trim()
             .isLength({min: 4, max:20})
     ]
-, (req: Request, res: Response) => {
+, validateRequest,(req: Request, res: Response) => {
     const {email, password } = req.body
-    const errors = validationResult(req)
-    if(!errors.isEmpty()) {
-       throw new RequestValidationError(errors.array())
-    }
+
     res.send({})
 })
 
