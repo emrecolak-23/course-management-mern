57ed2b7813b01372758a648fa8b9111bfdc74854
Emre Çolak <emrecolak@Emre.home>
2023-03-27 21:47:08 +0300

    check validation for signup route handler

--- server/src/routes/auth/signup.ts
+++ server/src/routes/auth/signup.ts
@@ -1,10 +1,24 @@
 // Import Dependencies
 import express, {Request, Response} from 'express'
-
+import { body, validationResult } from 'express-validator'
 // Create router
 const router = express.Router()
 
-router.post('/api/users/signup', (req: Request, res: Response) => {
+router.post('/api/users/signup', 
+    [
+        body('email')
+            .isEmail()
+            .withMessage('Email must be valid'),
+        body('password')
+            .trim()
+            .isLength({min: 4, max:20})
+    ]
+, (req: Request, res: Response) => {
+    const {email, password } = req.body
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+        return res.send(errors.array())
+    }
     res.send({})
 })
 
